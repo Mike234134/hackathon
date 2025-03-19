08be2629052e24c9603e568439abe09b5c23c8f1
Michael Mosuro <mimos3@morgan.edu>
2025-03-19 01:07:23 -0400

    new changes

--- hackathon/src/pages/Signup.jsx
+++ hackathon/src/pages/Signup.jsx
@@ -3,33 +3,76 @@ import React from "react"
 import { useNavigate } from 'react-router-dom'
 import './Signup.css'
 const Signup=()=> {
-    const [message,setMessage]=useState("");
-    const handlechange =(e)=>{
-        e.preventDefault();
-        alert("here")
+    
+    const [inputMessage,setInputMessage]=useState({
+        email:"",
+        name:"",
+        password:"",
+        address:"",
         
-    }
+    });
+    const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+    const[isDisabled,SetDisabled]=useState(false);
+    
     const navigate = useNavigate();
     const handleExplore = ()=>{
-        navigate("/home")
+        navigate("/login")
     }
+    const HandleSignup =(e)=>{
+        e.preventDefault()
+        console.log(inputMessage)
+    }
+    const ValidSignup = ()=>{
+         if(inputMessage){
 
+         }
+        
+    }
     return (
       <>
         <div>
             <h1>Sign Up for a Account</h1>
-         <form>
+         <form
+         onSubmit={HandleSignup} 
+         
+         >
             <input
-            type="text"
-            value={message} 
-            onChange={(e)=> setMessage(e.target.value)}></input>
-            <button onClick={handlechange}></button>
-           <div className="login">
+            type="email"
+            value={inputMessage.email} 
+            required={true}
+            onChange={(e)=> setInputMessage((prev=>({...prev,email:e.target.value})))}/>
+            
+           
+            
+             <input
+             type="text"
+             value={inputMessage.name} 
+             required={true}
+             onChange={(e)=> setInputMessage((prev=>({...prev,name:e.target.value})))}
+             />
             <input
-            type="text" 
-            ></input>
-            <button onClick={handleExplore}>If you already have an account</button>
-            </div>
+             type="password"
+             value={inputMessage.password} 
+             required={true}
+             onChange={(e)=> setInputMessage((prev=>({...prev,password:e.target.value})))}
+            />
+            <input type="address"
+             value={inputMessage.address} 
+             required={true}
+             onChange={(e)=> { setIsDropdownVisible(e.target.value.length > 0);setInputMessage((prev=>({...prev,address:e.target.value})))}}
+             onBlur={() => setTimeout(() => setIsDropdownVisible(false), 100)} // Optional: hide dropdown on blur
+          onFocus={() => inputMessage.address.length > 0 && setIsDropdownVisible(true)} // Show on focus if there is input
+        />
+        {isDropdownVisible && (
+          <ul>
+              <li>
+                {"something"}
+              </li>
+          </ul>
+        )}
+            
+            <input type="submit" disabled={isDisabled} value="sign up"/>
+            <button  onClick={handleExplore}> Click here if you already have a account</button>
          </form>
         </div>
       </>
